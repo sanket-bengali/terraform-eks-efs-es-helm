bfa48d7a7cd3e0a4abc1d704b3995cf56f9f201a
sanket-bengali <sanketbengali.23@gmail.com>
2019-06-18 22:43:48 +0100

    Update my_app.tf

--- my_app.tf
+++ my_app.tf
@@ -1,15 +1,40 @@
+provider "aws" {
+  ...
+}
+module "label" {
+  ...
+}
+module "vpc" {
+  ...
+}
+module "eks-cluster" {
+  ...
+}
+.....
+.....
+locals {
+  kubeconfig_filename          = "${path.module}/kubeconfig${var.delimiter}${module.eks_cluster.eks_cluster_id}.yaml"
+}
 module "my_app_eks_setup" {
   source                        = "/path/to/module/my-app-eks-setup"
   namespace                     = "${var.namespace}"
+  
+  # EKS cluster name and endpoint
   cluster_name                  = "${module.cluster_label.id}"
   cluster_endpoint              = "${module.eks_cluster.eks_cluster_endpoint}"
+  
   kubeconfig                    = "${module.eks_workers.kubeconfig}"
   kubeconfig_filename           = "${local.kubeconfig_filename}"
   cluster_ca_certificate        = "${base64decode(module.eks_cluster.eks_cluster_certificate_authority_data)}"
+  # If generated token is used for Kubernetes and/or Helm providers
   #token                         = "${module.eks_cluster.aws_authenticator_token}"
-  domain_endpoint               = "${module.elasticsearch.domain_endpoint}"
   aws_iam_role                  = "${module.eks_workers.worker_role_arn}"
-
+  
+  # Elasticsearch domain endpoint
+  domain_endpoint               = "${module.elasticsearch.domain_endpoint}"
+  
+  # Helm chart params for efs-provisioner
   efs_id                        = "${module.efs.id}"
   region                        = "${var.region}"
 }
+.....
